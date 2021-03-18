ac65f4d9abb8ed9e265a02889e24892779bf1595
Itsmesammy08 <sk.amador0879@student.tsu.edu.ph>
2021-03-18 16:52:26 +0800

    Samantha Kaye Amador

---
+++ Employeeinfo6.java
@@ -0,0 +1,21 @@
+package ipt2casestudygroup5;
+public class EmployeeInfo6 {
+        public void getempinfoname() {
+        System.out.println("Samantha Kaye A. Amador");
+}
+        public void getempinfoposi() {
+        System.out.println("Human resource personnel");
+}
+        public void getempinfostre() {
+        System.out.println("Active Listening Skills");
+}
+        public void getempinfoweak() {
+        System.out.println("Taking credit for group projects");
+}
+        public void getempinfocriminalrec() {
+        System.out.println("None");
+}
+        public void getempinfoperprevorga() {
+        System.out.println("Very well");
+}
+}
