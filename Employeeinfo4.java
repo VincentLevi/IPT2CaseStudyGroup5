517d82ee33b56853031f1b5b0c7f762faa924eb1
cruzjeric <j.cruz1454@student.tsu.edu.ph>
2021-03-18 16:41:25 +0800

    Jeric Cruz

---
+++ Employeeinfo4.java
@@ -0,0 +1,21 @@
+package ipt2casestudygroup5;
+public class EmployeeInfo4 {
+        public void getempinfoname() {
+        System.out.println("Jeric G. Cruz");
+}
+        public void getempinfoposi() {
+        System.out.println("Marketing specialist");
+}
+        public void getempinfostre() {
+        System.out.println("Organizational Skills");
+}
+        public void getempinfoweak() {
+        System.out.println("Providing too much detail in reports.");
+}
+        public void getempinfocriminalrec() {
+        System.out.println("None");
+}
+        public void getempinfoperprevorga() {
+        System.out.println("Good");
+}
+}
