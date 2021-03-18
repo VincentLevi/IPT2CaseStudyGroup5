ad766c95c1230d1994d7361ed68cb58c99480733
DexCN31 <d.nocum2546@student.tsu.edu.ph>
2021-03-18 16:34:32 +0800

    Dexter Nocum

---
+++ Employeeinfo3.java
@@ -0,0 +1,21 @@
+package ipt2casestudygroup5;
+public class EmployeeInfo3 {
+        public void getempinfoname() {
+        System.out.println("Dexter C. Nocum");
+}
+        public void getempinfoposi() {
+        System.out.println("Executive assistants");
+}
+        public void getempinfostre() {
+        System.out.println("Leadership Skills");
+}
+        public void getempinfoweak() {
+        System.out.println("Leaving projects unfinished");
+}
+        public void getempinfocriminalrec() {
+        System.out.println("None");
+}
+        public void getempinfoperprevorga() {
+        System.out.println("Poor");
+}
+}
