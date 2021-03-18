df5fb33e42bc588fd86da7bd53f17377f0b4b558
VincentLevi <vl.olonan0646@student.tsu.edu.ph>
2021-03-18 16:18:20 +0800

    Vincent Levi Olonan

---
+++ Employeeinfo1.java
@@ -0,0 +1,21 @@
+package ipt2casestudygroup5;
+public class EmployeeInfo1 {
+        public void getempinfoname() {
+        System.out.println("Vincent Levi M. Olonan");
+}
+        public void getempinfoposi() {
+        System.out.println("Chief Technology Officer (CTO)");
+}
+        public void getempinfostre() {
+        System.out.println("Loyalty and Hard working employee");
+}
+        public void getempinfoweak() {
+        System.out.println("Taking on too much responsibility");
+}
+        public void getempinfocriminalrec() {
+        System.out.println("None");
+}
+        public void getempinfoperprevorga() {
+        System.out.println("Very well");
+}
+}
