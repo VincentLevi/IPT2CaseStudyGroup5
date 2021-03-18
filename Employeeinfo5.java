5ac7fb75351a26664624b16f06afe1162a57b59b
AnaLuzAgnote <al.agnote1616@student.tsu.edu.ph>
2021-03-18 16:47:12 +0800

    Ana Luz Agnote

---
+++ Employeeinfo5.java
@@ -0,0 +1,21 @@
+package ipt2casestudygroup5;
+public class EmployeeInfo5{
+        public void getempinfoname() {
+        System.out.println("Ana Luz A. Agnote");
+}
+        public void getempinfoposi() {
+        System.out.println("Business analyst");
+}
+        public void getempinfostre() {
+        System.out.println("Excellent Written and Verbal Communication");
+}
+        public void getempinfoweak() {
+        System.out.println("Shifting from one project to another (multitasking)");
+}
+        public void getempinfocriminalrec() {
+        System.out.println("None");
+}
+        public void getempinfoperprevorga() {
+        System.out.println("Great");
+}
+}
