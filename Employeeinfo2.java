44ce44b861b16b54e1783c86a9e575b5acc23c38
Abrazado1228 <j.abrazado1599@student.tsu.edu.ph>
2021-03-18 16:26:07 +0800

    Esrael D. Abrazo

---
+++ Employeeinfo2.java
@@ -0,0 +1,21 @@
+package ipt2casestudygroup5;
+public class EmployeeInfo2 {
+        public void getempinfoname() {
+        System.out.println("Esrael D. Abrazo");
+}
+        public void getempinfoposi() {
+        System.out.println("Operations and Production");
+}
+        public void getempinfostre() {
+        System.out.println("Great in Logic");
+}
+        public void getempinfoweak() {
+        System.out.println("Lack of Experience");
+}
+        public void getempinfocriminalrec() {
+        System.out.println("None");
+}
+        public void getempinfoperprevorga() {
+        System.out.println("Great");
+}
+}
