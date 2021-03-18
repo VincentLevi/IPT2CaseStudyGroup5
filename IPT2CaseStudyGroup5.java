a5f52126fe1add14815f728d0bdce7abb52e5823
Abrazado1228 <j.abrazado1599@student.tsu.edu.ph>
2021-03-18 15:04:41 +0800

    Jesrael Abrazado

--- IPT2CaseStudyGroup5.java
+++ IPT2CaseStudyGroup5.java
@@ -15,26 +15,43 @@ public class IPT2CaseStudyGroup5 {
         System.out.print("Name: ");
         if(choice.equals("1")){
          Employ1.getempinfoname();
-        }   
+        } 
+                if(choice.equals("2")){
+         Employ1.getempinfoname();
+        }  
                 System.out.print("Position: ");
         if(choice.equals("1")){
          Employ1.getempinfoposi();
         }   
+                if(choice.equals("2")){
+         Employ1.getempinfoposi();
+        }
                 System.out.print("Strenght: ");
         if(choice.equals("1")){
          Employ1.getempinfostre();
         }   
+                if(choice.equals("2")){
+         Employ1.getempinfostre();
+        }
                 System.out.print("Weakness: ");
         if(choice.equals("1")){
          Employ1.getempinfoweak();
         }   
+                if(choice.equals("2")){
+         Employ1.getempinfoweak();
+        }
                 System.out.print("Criminal Record: ");
         if(choice.equals("1")){
          Employ1.getempinfocriminalrec();
         }   
-                System.out.print("Performance in Previous Organization: ");
+                if(choice.equals("2")){
+         Employ1.getempinfocriminalrec();
+        }                  System.out.print("Performance in Previous Organization: ");
         if(choice.equals("1")){
          Employ1.getempinfoperprevorga();
         }     
+                if(choice.equals("2")){
+         Employ1.getempinfoperprevorga();
+        } 
     }
 }
