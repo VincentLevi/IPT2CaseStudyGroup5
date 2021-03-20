c1085f00e6f98fb8b449cddaba99322bbb0288ba
AnaLuzAgnote <al.agnote1616@student.tsu.edu.ph>
2021-03-20 20:03:58 +0800

    Samantha Kaye A. Amador

--- Employeeinfo6.java
+++ Employeeinfo6.java
@@ -1,7 +1,19 @@
 package ipt2casestudygroup5;
 public class EmployeeInfo6 {
         public void getempinfoname() {
-        System.out.println("Samantha Kaye A. Amador");
+        System.out.println("Samantha Kaye");
+}
+	public void getempinfoLastname() {
+        System.out.println("Amador");
+}
+	public void getempinfoMiddlename() {
+        System.out.println("Austria");
+}
+	public void getempinfoage() {
+        System.out.println("21yrs.old");
+}
+	public void getempinfoaddress() {
+        System.out.println("Nueva Ecija");
 }
         public void getempinfoposi() {
         System.out.println("Human resource personnel");
